2b8a9c28a830235d4e554b8b7d4aaf102d1e9584
vincent <somanou.vincent@gmail.com>
2023-02-18 17:33:29 +0100

    first commit

---
+++ VirusGame/VirusGameApp.swift
@@ -0,0 +1,20 @@
+//
+//  VirusGameApp.swift
+//  VirusGame
+//
+//  Created by Vince on 18/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct VirusGameApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
